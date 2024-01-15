b08993a95aa1b3951f4784e9470cbf5c0b2bc217
Jeries Abedrabbo <jabed001@odu.edu>
2024-01-15 18:30:58 -0500

    Adding Initial Version of Jeries Abedrabbo's Personal Website v0.1.0

---
+++ src/components/NotFound.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import { ref } from 'vue'
+  const curr_year = ref(new Date().getFullYear())
+  const ss = ref('text-warning')
+  document.title = "404 Not Found!"
+</script>
+
+<template>
+
+    <!-- Row for any alerts on top of page -->
+    <div class="row">
+      <div class="col">
+        <div class="alert alert-primary">
+          <strong>Uh oh!</strong> 404 Page Not Found :(
+        </div>             
+      </div>
+    </div>
+
+    <!-- Main content -->
+    <div class="row text-center my-5">
+      <div class="col">
+        <h1 :class="ss">Why you dooo diss!!??</h1>
+        <img class="img-fluid w-50" style="object-fit:fill;" src="/again.gif" alt="Picture of man saying again, because you keep messing up and coming here.. sheesh! He's in space BTW">
+      </div>
+    </div>
+
+    <!-- Footer content -->
+    <div class="row fixed-bottom">
+      <div class="col text-center">
+        <small class="bg-primary text-warning">Copyright Reserved &copy; {{curr_year}} - Jeries Abedrabbo</small>
+      </div>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/PersonalPage.vue
@@ -0,0 +1,119 @@
+<script setup>
+  import { onMounted, ref } from 'vue'
+  const curr_year = ref(new Date().getFullYear())
+  const email = ref('mailto:jeriesabedrabbo955@msn.com')
+  const bg_music = ref('bg_music')
+  onMounted(() => {
+    console.log("Mounted, now time to play music!")
+    // var audio = new Audio('/interstellar_nocopyright.mp3')
+    // document.getElementById(bg_music.value).muted = "true"
+    document.getElementById(bg_music.value).pause()
+    document.getElementById(bg_music.value).play()
+
+  })
+  
+  function playMusic() {
+    console.log("Playing now after click!")
+    document.getElementById(bg_music.value).play()
+  }
+</script>
+
+<template>
+
+    <!-- Row for any alerts on top of page -->
+    <!-- Added w-100 to class row in this div, because it seems to remove the horizontal overflow scroll bar -->
+    <div class="row w-100">
+      <div class="col">
+        <!-- <div class="alert alert-danger">
+          <strong>Problem!</strong> This is a placeholder for any new alers that might happen.
+        </div>       -->
+        <audio class="h-75" :id="bg_music" controls autoplay loop>
+          <source src="/interstellar_nocopyright.mp3" type="audio/mpeg">
+          <div class="alert alert-warning">
+            <strong>Problem!</strong> Your browser does not support the audio element.
+          </div>                
+        </audio>        
+      </div>
+    </div>
+
+    <!-- Main content -->
+    <div @click="playMusic" class="row m-5">
+      <div class="col-2">
+        <div class="row">
+          <div class="col">
+            <img class="img-fluid" style="border-style: inset; border-width: 4px; border-color: yellow; " src="/me.jpg" alt="A Picture of me, myself, and I">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col text-center">
+            <strong style="color: aqua;">
+              Handsome me at the cosmosphere museum Hutchinson, KS
+            </strong>
+          </div>
+        </div>
+      </div>
+      <div class="col-10 border-start border-2">
+        <h1 class="w-100 text-danger" style="font-family:'Times New Roman'">Who am I?</h1>
+        <p class="justify" style="font-family:'Times New Roman'; color: white;">
+          My name is Jeries Abedrabbo, and I'm a Senior Software Engineer and an Aerospace Engineer!
+          <br>
+          I'm interested in technology, computers, and SPACE! This webpage is a simple creation of mine to tell a little about myself.
+          <br>
+          As you can see, this page looks antique, but that's just the way I wanted it to be. The Internet in the late 1990s and early 2000s
+          was a different beast. It was very cool to see all these cool webpages with colors all over the place, it gives me a warm sense
+          of nostalgia, hence why I created this page to be like that. Well, I know you might not like it that way, but, ehh, it is what it is...
+          <br>
+          Please, be my guest, and take a look at the different sections of my personal website. Contact me if you need help, or if you want me to 
+          help you with your next big idea, and if you have a million dollars you want to give me, this is an all inclusive offer.
+          <br>
+          Okay, enough talking! Dive in, pretty boy!
+          <br>
+          But before you go, please, have a coffee with me <img src="https://web.archive.org/web/20040715211000/http://www.geocities.com/two4dramma/cupsteamincoffee_tea.gif" alt="">  
+        </p>
+        <ul>
+          <li style="list-style-image: url('/favicons/icons8-comet-emoji-favicons12/Web/icons8-comet-emoji-16.png')">
+            <a  href="#/studies">What have I studied?</a>
+          </li>
+          <li style="list-style-image: url('/favicons/icons8-person-astronaut-emoji-favicons19/Web/icons8-person-astronaut-emoji-16.png');">
+            <a  href="#/whatido">What do I do?</a>
+          </li>
+          <li style="list-style-image: url('/favicons/icons8-venus-planet-color-favicons14/Web/icons8-venus-planet-color-16.png');">
+            <a href="#/myprojects">Some projects of mine <img src="/ezgif.com-resize.gif" alt="fire GIF" ></a>
+          </li>          
+        </ul>
+        <!-- <div class="row">
+          <div class="col">
+            <li style="list-style-image: url('/favicons/icons8-comet-emoji-favicons12/Web/icons8-comet-emoji-16.png')">
+              <a  href="#">What have I studied?</a>
+            </li>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <li style="list-style-image: url('/favicons/icons8-person-astronaut-emoji-favicons19/Web/icons8-person-astronaut-emoji-16.png');">
+              <a  href="#">What do I do?</a>
+            </li>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <li style="list-style-image: url('/favicons/icons8-venus-planet-color-favicons14/Web/icons8-venus-planet-color-16.png');">
+              <a href="#">Some projects of mine <img src="/ezgif.com-resize.gif" alt="fire GIF" ></a>
+            </li>
+          </div>
+        </div> -->
+      </div>
+    </div>
+
+    <!-- Footer content -->
+    <div class="row fixed-bottom">
+      <div class="col text-center">
+        <small class="bg-primary text-warning">Copyright Reserved &copy; {{curr_year}} - <a :href="email" class="text-warning">Jeries Abedrabbo</a></small>
+      </div>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
